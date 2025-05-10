d50d8c4bb3ecfea4f6f2b075092f6404f9f87555
adelac01 <adelac01@student.ubc.ca>
2025-05-10 16:03:35 -0700

    Implemented resolution parameter, adjusting the number of words in memory dependent on selected resolution

--- rtl/vga_interface.sv
+++ rtl/vga_interface.sv
@@ -1,14 +1,14 @@
-module vga_interface(input logic clk, input logic rst_n, output logic pll_lock, 
-
-                     input logic [7:0] r_in, input logic [7:0] g_in, input logic [7:0] b_in, 
-                     input logic unsigned [8:0] x_in, input logic unsigned [7:0] y_in,
-
-                     // VGA Signals
-                     output logic [7:0] VGA_R, output logic [7:0] VGA_G, output logic [7:0] VGA_B,
-                     output logic VGA_HS, output logic VGA_VS, output logic VGA_SYNC_N,
-                     output logic VGA_CLK, output logic VGA_BLANK_N);
+module vga_interface(
+    clk, rst_n, pll_lock,
+    r_in, g_in, b_in,
+    x_in, y_in,
+    VGA_R, VGA_G, VGA_B,
+    VGA_HS, VGA_VS, VGA_SYNC_N,
+    VGA_CLK, VGA_BLANK_N,
+);
     
-    // parameter RESOLUTION = "640x480";
+    parameter RESOLUTION = "320x240";
+    parameter CHANNEL_SIZES = 8;
     parameter MONOCHROME = "true";
 
     // Horizotal count thresholds (pixel cycles)
@@ -23,52 +23,112 @@ module vga_interface(input logic clk, input logic rst_n, output logic pll_lock,
     parameter V_SYNC_END = 491;
     parameter V_BACK_PORCH_END = 524;
 
-    // Pixel count and line count
-    int p_count, l_count;
+    parameter x_in_width = ((RESOLUTION == "160x120") ? 8 : ((RESOLUTION == "320x240") ? 9 : 10));
+    parameter y_in_width = ((RESOLUTION == "160x120") ? 8 : ((RESOLUTION == "320x240") ? 8 : 9));
+    parameter num_words = ((RESOLUTION == "160x120") ? 19200 : ((RESOLUTION == "320x240") ? 76800 : 307200)); 
+    parameter address_size = ((RESOLUTION == "160x120") ? 15 : ((RESOLUTION == "320x240") ? 17 : 19)); 
+    parameter screen_width = ((RESOLUTION == "160x120") ? 160 : ((RESOLUTION == "320x240") ? 320 : 640));
+    parameter screen_height = ((RESOLUTION == "160x120") ? 120 : ((RESOLUTION == "320x240") ? 240 : 480));
+    parameter shift_amount = ((RESOLUTION == "160x120") ? 2 : ((RESOLUTION == "320x240") ? 1 : 0));
+
+    input logic clk;
+    input logic rst_n;
+    output logic pll_lock;
+
+    // TODO: Change bus width depending on user desired channel widths
+    input logic [7:0] r_in;
+    input logic [7:0] g_in;
+    input logic [7:0] b_in;
+
+    input logic [x_in_width-1:0] x_in;
+    input logic [y_in_width-1:0] y_in;
+
+    // VGA Signals
+    output logic [7:0] VGA_R;
+    output logic [7:0] VGA_G;
+    output logic [7:0] VGA_B;
+    output logic VGA_HS;
+    output logic VGA_VS;
+    output logic VGA_SYNC_N;
+    output logic VGA_CLK;
+    output logic VGA_BLANK_N;
+
+    logic [9:0] p_count;
+    logic [9:0] l_count;
     logic vga_clk, count_en;
     logic wren, rden;
 
-    // 640x480 resolution
-    // wire [18:0] rdaddress = l_count * 640 + p_count;
-    // wire [18:0] wraddress = y_in * 640 + x_in;
+    logic [address_size-1:0] buffer_read_address;
+    assign buffer_read_address = (l_count >> shift_amount) * screen_width + (p_count >> shift_amount);
+    logic [address_size-1:0] buffer_write_address;
+    assign buffer_write_address = y_in * screen_width + x_in;
 
-    // 320x240 resolution
-    // wire [16:0] rdaddress = (l_count >> 1) * 320 + (p_count >> 1);
-    // wire [16:0] wraddress = y_in * 320 + x_in;
+    // logic [7:0] color_data;
+    // assign VGA_R = MONOCHROME == "true" ? color_data;
+    // assign VGA_G = MONOCHROME == "true" ? color_data;
+    // assign VGA_B = MONOCHROME == "true" ? color_data;
 
-    // 160x120 resolution
-    wire [14:0] rdaddress = (l_count >> 2) * 160 + (p_count >> 2);
-    wire [14:0] wraddress = y_in * 160 + x_in;
+    logic [7:0] color_output;
+    assign VGA_R = color_output;
+    assign VGA_G = color_output;
+    assign VGA_B = color_output;
 
-    logic [7:0] monochrome_data;
-    assign VGA_R = monochrome_data;
-    assign VGA_G = monochrome_data;
-    assign VGA_B = monochrome_data;
-    // vga_test_rom grayscale_rom(.clock(vga_clk), .address(rdaddress), .rden(rden), .q(monochrome_data));
-    // vga_ram ram_buffer(.wrclock(clk), .rdclock(vga_clk), .wraddress(wraddress), .rdaddress(rdaddress), .wren(wren), .rden(rden), .data(r_in), .q(monochrome_data));
-    small_buf ram_buffer(
-        .clock_a(clk), 
-        .clock_b(vga_clk), 
-        .address_a(wraddress), 
-        .address_b(rdaddress), 
-        .data_a(r_in), 
-        // .data_b(),
-        .rden_a(1'b0),
+    altsyncram pixel_buffer(
+        .address_a(buffer_write_address),
+        .address_b(buffer_read_address),
+        .clock0(clk),
+        .clock1(vga_clk),
+        .data_a(r_in),
+        .data_b (8'b1111_1111),
+        .rden_a(1'b1),
         .rden_b(rden),
         .wren_a(wren),
         .wren_b(1'b0),
-        // .q_a(),
-        .q_b(monochrome_data)
+        .q_a(),
+        .q_b(color_output),
+        .aclr0(1'b0),
+        .aclr1(1'b0),
+        .addressstall_a(1'b0),
+        .addressstall_b(1'b0),
+        .byteena_a(1'b1),
+        .byteena_b(1'b1),
+        .clocken0(1'b1),
+        .clocken1(1'b1),
+        .clocken2(1'b1),
+        .clocken3(1'b1),
+        .eccstatus(),  
     );
-    debug_mem debug(.address(wraddress), .clock(clk), .data(r_in), .wren(wren));
+    defparam
+        pixel_buffer.address_aclr_b = "NONE",
+        pixel_buffer.address_reg_b = "CLOCK1",
+        pixel_buffer.clock_enable_input_a = "BYPASS",
+        pixel_buffer.clock_enable_input_b = "BYPASS",
+        pixel_buffer.clock_enable_output_b = "BYPASS",
+        pixel_buffer.init_file = "",
+        pixel_buffer.intended_device_family = "Cyclone V",
+        pixel_buffer.lpm_type = "altsyncram",
+        pixel_buffer.numwords_a = num_words,
+        pixel_buffer.numwords_b = num_words,
+        pixel_buffer.operation_mode = "DUAL_PORT",
+        pixel_buffer.outdata_aclr_b = "NONE",
+        pixel_buffer.outdata_reg_b = "UNREGISTERED",
+        pixel_buffer.power_up_uninitialized = "FALSE",
+        pixel_buffer.ram_block_type = "M10K",
+        pixel_buffer.rdcontrol_reg_b = "CLOCK1",
+        pixel_buffer.widthad_a = address_size,
+        pixel_buffer.widthad_b = address_size,
+        pixel_buffer.width_a = 8,
+        pixel_buffer.width_b = 8,
+        pixel_buffer.width_byteena_a = 1;
+
+    // debug_mem debug(.address(wraddress), .clock(clk), .data(r_in), .wren(wren));
 
     // Use PLL to turn input clk into 25.175MHz
     vga_pll pll(.refclk(clk), .rst(~rst_n), .outclk_0(vga_clk), .locked(pll_lock));
 
     assign VGA_SYNC_N = 0; // Should always be low
     assign VGA_CLK = vga_clk;
-    // assign wren = (x_in < 320 && y_in < 240);
-    assign wren = 1'b1;
+    assign wren = (x_in < screen_width && y_in < screen_height);
     assign rden = (p_count <= H_VISIBLE_END && l_count <= V_VISIBLE_END);
 
     always_ff @(posedge vga_clk or negedge rst_n) begin
